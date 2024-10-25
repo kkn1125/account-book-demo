558946ec8d366620611a6c20d4d5921e58a9bed2
kkn1125 <xpfjrnseks1@naver.com>
2024-10-25 10:54:45 +0900

    ✨ fix: eslint any 허용 + firebase 테스트

--- eslint.config.js
+++ eslint.config.js
@@ -26,5 +26,6 @@ export default tseslint.config(
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-unused-expressions": "off",
     },
+    "@typescript-eslint/no-explicit-any": "off",
   }
 );
